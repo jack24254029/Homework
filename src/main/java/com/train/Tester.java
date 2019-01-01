fb96fccd970a464f92e7887cbd094f072f8e9285
Shun-Min Chang <ji394m6y7@gmail.com>
2019-01-01 22:52:32 +0800

    Refactor java code and kotlin code

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -6,7 +6,7 @@ public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int step = 1, ticketsCount = 0, roundTripCount = 0;
-        while (step != 3) {
+        while (step <= 3) {
             switch (step) {
                 case 1:
                     System.out.print("Please enter number of tickets: ");
@@ -26,9 +26,12 @@ public class Tester {
                         System.out.println("Number have to more then 0 and less than number of tickets.");
                     }
                     break;
+                case 3:
+                    JavaTicket ticket = new JavaTicket(ticketsCount, roundTripCount);
+                    ticket.calculate();
+                    step++;
+                    break;
             }
         }
-        JavaTicket ticket = new JavaTicket(ticketsCount, roundTripCount);
-        ticket.calculate();
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -4,10 +4,34 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets: ")
-    val totalTicketCount = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    val roundTripTicketCount = scanner.nextInt()
-    val ticket = KotlinTicket(totalTicketCount, roundTripTicketCount)
-    ticket.calculate()
+    var step = 1
+    var totalTicketCount = 0
+    var roundTripTicketCount = 0
+    while (step <= 3) {
+        when (step) {
+            1 -> {
+                print("Please enter number of tickets: ")
+                totalTicketCount = scanner.nextInt()
+                if (totalTicketCount > 0) {
+                    step += 1
+                } else {
+                    println("Number have to more than 0.")
+                }
+            }
+            2 -> {
+                print("How many round-trip tickets: ")
+                roundTripTicketCount = scanner.nextInt()
+                if (roundTripTicketCount in 1..totalTicketCount) {
+                    step += 1
+                } else {
+                    println("Number have to more then 0 and less than number of tickets.")
+                }
+            }
+            3 -> {
+                val ticket = KotlinTicket(totalTicketCount, roundTripTicketCount)
+                ticket.calculate()
+                step += 1
+            }
+        }
+    }
 }
